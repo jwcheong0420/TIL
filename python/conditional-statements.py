4a057862d8a08fbd4f19d760132288acff00d835
Joowon Cheong <jwcheong0420@gmail.com>
2019-02-05 16:54:55 +0900

    [Python] 생활코딩 Python 변수 ~ 입력과 출력(#1)

---
+++ python/conditional-statements.py
@@ -0,0 +1,9 @@
+input = 22
+real_joowon = 11
+real_k8805 = "ab"
+if real_joowon == input:
+	print("Hello! joowon")
+elif real_k8805 == input:
+	print("Hello! k8805")
+else:
+	print("Who are you")
---
+++ python/variable.py
@@ -0,0 +1,18 @@
+print("=== Variable ===")
+x = 10
+y = 5
+print(x + y)
+title = "python"
+print("Title is " + title)
+
+print("=== String variable ===")
+name = "Joowon"
+print("안녕하세요. " + name + "님")
+print(name + "님을 위한 강의를 준비했습니다.")
+
+
+print("=== Number variable ===")
+donation = 200
+student = 5
+sponsor = 20
+print((donation * student) / sponsor)
