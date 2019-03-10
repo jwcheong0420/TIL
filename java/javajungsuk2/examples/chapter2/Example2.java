cccba3f8361cd79bdd1cb9c5109208caa184808c
Joowon Cheong <jwcheong0420@gmail.com>
2019-03-11 04:20:21 +0900

    [Java] Java의 정석2 - Chapter2 연습문제 (#4)

---
+++ java/javajungsuk2/examples/chapter2/Example2.java
@@ -0,0 +1,5 @@
+package javajungsuk2.chapter2;
+
+public class Example2 {
+	long regNo = 9304202222222l;	// 주민등록번호는 13자리, int는 대략 10자리까지만 표현 가능
+}
---
+++ java/javajungsuk2/examples/chapter2/Example5.java
@@ -0,0 +1,9 @@
+package javajungsuk2.chapter2;
+
+public class Example5 {
+	public static void main(String[] args) {
+		System.out.println('A' + 'B');	// 65 + 66 = 131
+		System.out.println('1' + 2);	// 49 + 2 = 51
+		System.out.println('1' + '2');	// 49 + 50 = 99
+	}
+}
