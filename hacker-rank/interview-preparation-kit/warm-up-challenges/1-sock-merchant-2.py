9f8f518228035f17893132f6b92f45a27b39426a
Joowon Cheong <jwcheong0420@gmail.com>
2019-11-06 00:08:13 +0900

    [HackerRank] Easy 1 - Sock Merchant - retry

---
+++ hacker-rank/interview-preparation-kit/warm-up-challenges/1-sock-merchant-2.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    nums = dict(Counter(ar))
+    pairs = 0
+    for v in nums.values():
+        pairs += (v//2)
+
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
