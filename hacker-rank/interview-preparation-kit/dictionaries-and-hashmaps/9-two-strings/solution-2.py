71cbc13981026ff70a302bb3d2d9ecbd4e8adfb2
Joowon Cheong <joowon_cheong@tmax.co.kr>
2019-11-27 17:56:45 +0900

    [HackerRank] 9-two-strings

---
+++ hacker-rank/interview-preparation-kit/dictionaries-and-hashmaps/9-two-strings/solution-2.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the twoStrings function below.
+def twoStrings(s1, s2):
+    s1_set = set(s1)
+    s2_set = set(s2)
+
+    if s1_set & s2_set:
+        return "YES"
+    else:
+        return "NO"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
