d56af6e2e794151011ff0fd61b31a94fa156644f
Joowon Cheong <jwcheong0420@gmail.com>
2020-02-03 02:37:33 +0900

    [Programmers] Sort 2nd problem

---
+++ practice-coding/programmers/test-kit/02-sort/2-level2/solution1.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    private class Num implements Comparable<Num> {
+        String numStr;
+        Num (int num) {
+            this.numStr = String.valueOf(num);
+        }
+
+        @Override
+        public int compareTo(Num o) {
+
+            if (this.numStr.length() == o.numStr.length()) {
+                return this.numStr.compareTo(o.numStr) * (-1);
+            } else {
+                String num1 = this.numStr + o.numStr;
+                String num2 = o.numStr + this.numStr;
+                return num1.compareTo(num2) * (-1);
+            }
+        }
+    }
+
+    public String solution(int[] numbers) {
+        List<Num> numList = new ArrayList<>();
+        for (int num : numbers) {
+            numList.add(new Num(num));
+        }
+        Collections.sort(numList);
+
+
+        String answer = "";
+        for (Num num : numList) {
+            answer += num.numStr;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] numbers = {3, 30, 34, 5, 9};
+
+        String answer = sol.solution(numbers);
+        System.out.println("answer : " + answer);
+    }
+}
---
+++ practice-coding/programmers/test-kit/02-sort/2-level2/solution2.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Solution {
+    public String solution(int[] numbers) {
+        List<Integer> numList = new ArrayList<>();
+        for (int num : numbers) {
+            numList.add(num);
+        }
+
+        Collections.sort(numList, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                String num1 = o1.toString() + o2.toString();
+                String num2 = o2.toString() + o1.toString();
+                return num1.compareTo(num2) * (-1);
+            }
+        });
+
+        String answer = "";
+        for (Integer num : numList) {
+            answer += num.toString();
+        }
+
+        return answer.charAt(0) == '0' ? "0" : answer;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] numbers = {3, 30, 34, 5, 9};
+        // int[] numbers = {0, 0, 0, 0};
+
+        String answer = sol.solution(numbers);
+        System.out.println("answer : " + answer);
+    }
+}
